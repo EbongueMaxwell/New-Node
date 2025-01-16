e0861e37cb60ce4c4dc5a71431da6fa6317437e9
EbongueMaxwell <nsamemaxwell00@gmail.com>
2025-01-17 00:49:38 +0100

    Add files via upload

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,21 +1,26 @@
 const User = require('../libs/models/user.model');
-const createUser = async (req, res) => {
-    await User.create({
-    email: 'nathan@mail.com',
-    password: 'password',
-    });
-    res.render('user', { message: 'User Created', user: null });
+const bcrypt = require('bcrypt');
+
+const signup = async (req, res) => {
+  const { email, password } = req.body;
+  const query = { email };
+
+  const existingUser = await User.findOne(query);
+  if (existingUser) {
+    // Email aready exists
+    res.redirect('/signup');
+  } else {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = {
+      email,
+      password: hashedPassword,
     };
-    const getUser = async (req, res) => {
-    const user = await User.findOne({ email: 'nathan@mail.com' });
-    res.render('user', { message: 'User Retrieved', user: user });
-    };
-    const deleteUser = async (req, res) => {
-    await User.findOneAndDelete({ email: 'nathan@mail.com' });
-    res.render('user', { message: 'User Deleted', user: null });
-    };
-    module.exports = {
-    getUser,
-    createUser,
-    deleteUser,
-    };
+    const result = await User.create(user);
+    req.session.userId = result._id;
+    res.redirect('/dashboard');
+  }
+};
+
+module.exports = {
+  signup,
+};
